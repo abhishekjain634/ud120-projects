7cdf8191c849a3647ce73279303e702b7db8c01b
Abhishek Jain <abhishek.jain634@gmail.com>
2016-03-16 17:43:18 -0500

    Lesson 4 complete
    learnt Adaboost, Random Forest, and kNN

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,7 +3,10 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from time import time
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -15,7 +18,6 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
 #### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
@@ -25,18 +27,27 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
+plt.close()
+#plt.savefig("intialVis.png")
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+t0 = time()
+#clf =  AdaBoostClassifier(n_estimators=100)
+#clf = RandomForestClassifier(n_estimators=10000, criterion="entropy")
+clf = KNeighborsClassifier(n_neighbors = 5)
+clf.fit(features_train, labels_train)
+print "Training Time:", time()-t0
 
+t0 = time()
 
+pred = clf.predict(features_test)
+print "Prediction Time:", time()-t0
 
-
-
-
+print "Accuracy score:", accuracy_score(labels_test, pred)
 
 try:
     prettyPicture(clf, features_test, labels_test)
